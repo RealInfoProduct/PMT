917919a32a9f1dc226d12bb9bbc7404c19a02f71
DipakPansheriya <pansheriyadipak210@gmail.com>
2023-10-19 12:43:19 +0530

    api integration => platformmaster and ordermaster

--- src/app/web/ordermaster/ordermaster.component.ts
+++ src/app/web/ordermaster/ordermaster.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder } from '@angular/forms';
+import * as moment from 'moment';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { OrderList } from 'src/app/interface/AuthResponse';
+import { FirebaseService } from 'src/app/service/firebase.service';
+import { msgType } from 'src/assets/constant/message';
 
 @Component({
   selector: 'app-ordermaster',
@@ -9,12 +14,21 @@ import { FormGroup,FormBuilder } from '@angular/forms';
 export class OrdermasterComponent implements OnInit {
 
   orderform! : FormGroup
+  orderdId :any
+  isLoading :boolean = false
+  orderList : any
+  platFormList: any
 
-  constructor(private formbuilder : FormBuilder) { }
+
+
+  constructor(private formbuilder : FormBuilder,
+    private firebaseService :FirebaseService ,
+    private messageService : MessageService,
+    private confirmationService :ConfirmationService) { }
 
   ngOnInit(): void {
     this.orderdata()
-
+    this.getAllOrderList()
   }
 
   orderdata(){
@@ -26,9 +40,89 @@ export class OrdermasterComponent implements OnInit {
     })
   }
 
+  getAllOrderList(){
+    this.isLoading = true
+    this.firebaseService.getOrderList().subscribe((res: any) => {
+      if (res) {
+        this.orderList = res.filter((id: any) => id.loginId === localStorage.getItem('loginId'))        
+        console.log(this.orderList);
+        this.isLoading = false
+      }
+    })
 
+   this.firebaseService.getPlatformList().subscribe((res: any) => {
+      if (res) {
+        this.platFormList = res.filter((id: any) => id.loginId === localStorage.getItem('loginId'))        
+        this.isLoading = false
+      }
+    })
+  }
+
+  newOrder(){
+    this.orderform.reset()
+    this.orderdId = ''
+  }
+  
   ordermasterdata(){
-console.log(this.orderform.value);
+    const payload : OrderList = {
+      id: '',
+      loginId: localStorage.getItem('loginId'),
+      platFormName: this.orderform.value.platform.platFormName,
+      name: this.orderform.value.details,
+      date: moment(this.orderform.value.date).format(),
+      amount: this.orderform.value.amount
+    }
 
+    console.log(payload);
+
+    if (!this.orderdId) {
+      this.isLoading = true
+      this.firebaseService.addOrderList(payload).then((res: any) => {
+        this.messageService.add({
+          severity: msgType.success,
+          summary: 'Sucess',
+          detail: 'Data Add Successfully..',
+          life: 1500,
+        });
+        this.getAllOrderList()
+        this.isLoading = false
+      })
+    } else {
+      this.firebaseService.updateOrderList(this.orderdId, payload).then((res: any) => {
+        this.isLoading = true
+        this.messageService.add({
+          severity: msgType.success,
+          summary: 'Sucess',
+          detail: 'Data Update Successfully..',
+          life: 1500,
+        });
+        this.getAllOrderList()
+        this.isLoading = false
+      })
+    }
+  }
+
+
+  editData(data:any){
+    this.orderdId = data.id
+  }
+
+  deleteOrderlist(item: any) {
+    this.confirmationService.confirm({
+      message: 'Do you want to delete this record ?',
+      header: 'Delete Priority',
+      accept: async () => {
+        this.isLoading = true
+        this.firebaseService.deleteOrderList(item).then(res => {
+          this.messageService.add({
+            severity: msgType.success,
+            summary: 'Sucess',
+            detail: 'Data Delete Successfully..',
+            life: 1500,
+          });
+          this.isLoading = false
+        })
+      }
+    })
   }
 }
--- src/app/web/platformmaster/platformmaster.component.ts
+++ src/app/web/platformmaster/platformmaster.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder } from '@angular/forms';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { PlatformList } from 'src/app/interface/AuthResponse';
+import { FirebaseService } from 'src/app/service/firebase.service';
+import { msgType } from 'src/assets/constant/message';
 
 @Component({
   selector: 'app-platformmaster',
@@ -10,11 +14,21 @@ export class PlatformmasterComponent implements OnInit {
 
 
   platform!: FormGroup
+  platFormId :any
+  isLoading :boolean = false
+  platFormList : any
 
-  constructor(private formbuilder : FormBuilder) { }
+
+
+  constructor(private formbuilder : FormBuilder,
+    private firebaseService :FirebaseService ,
+    private messageService : MessageService,
+    private confirmationService :ConfirmationService) { }
 
   ngOnInit(): void {
     this.plateformmaster()
+    this.getAllPlatFormList()
+
   }
 
   plateformmaster(){
@@ -23,8 +37,82 @@ export class PlatformmasterComponent implements OnInit {
     })
   }
 
-  plateformData(){
-console.log(this.platform.value);
 
+  getAllPlatFormList(){
+    this.isLoading = true
+    this.firebaseService.getPlatformList().subscribe((res: any) => {
+      if (res) {
+        this.platFormList = res.filter((id: any) => id.loginId === localStorage.getItem('loginId'))        
+        console.log(this.platFormList);
+        
+        this.isLoading = false
+      }
+    })
+  }
+
+  newPlatForm(){
+    this.platform.reset()
+    this.platFormId = ''
+  }
+  
+  plateformData(){
+    const payload : PlatformList = {
+      id: '',
+      loginId: localStorage.getItem('loginId'),
+      platFormName: this.platform.value.platformname
+    }
+
+    console.log(payload);
+
+    if (!this.platFormId) {
+      this.isLoading = true
+      this.firebaseService.addPlatformList(payload).then((res: any) => {
+        this.messageService.add({
+          severity: msgType.success,
+          summary: 'Sucess',
+          detail: 'Data Add Successfully..',
+          life: 1500,
+        });
+        this.getAllPlatFormList()
+        this.isLoading = false
+      })
+    } else {
+      this.firebaseService.updatePlatformList(this.platFormId, payload).then((res: any) => {
+        this.isLoading = true
+        this.messageService.add({
+          severity: msgType.success,
+          summary: 'Sucess',
+          detail: 'Data Update Successfully..',
+          life: 1500,
+        });
+        this.getAllPlatFormList()
+        this.isLoading = false
+      })
+    }
+  }
+
+
+  editData(data:any){
+    this.platFormId = data.id
+    this.platform.controls['platformname'].setValue(data.platFormName)
+  }
+
+  deletePartylist(item: any) {
+    this.confirmationService.confirm({
+      message: 'Do you want to delete this record ?',
+      header: 'Delete Priority',
+      accept: async () => {
+        this.isLoading = true
+        this.firebaseService.deletePlatformList(item).then(res => {
+          this.messageService.add({
+            severity: msgType.success,
+            summary: 'Sucess',
+            detail: 'Data Delete Successfully..',
+            life: 1500,
+          });
+          this.isLoading = false
+        })
+      }
+    })
   }
 }
